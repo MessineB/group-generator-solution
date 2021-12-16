654d428a747deac75d1641c7e0b1ee648ca46072
MessineB <messinebelaroussi@gmail.com>
2021-12-16 11:08:41 +0100

    Second Commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,12 @@
 
 function addParticipant ()
 {
+    event.preventDefault();
+    //Je recupere l'input texte
+    const nameinputElt = document.getElementById('nameInput');
 
 //je créé mon prénom
-const participantName= "Prénom";
+const participantName= nameinputElt.value;
 //je créé un element li qui contient mon prenom
 const participantElt= `
 <li>${participantName}</li>
@@ -13,7 +16,9 @@ const participantElt= `
 const participantListElt=document.getElementById("participantList");
 //j'ajoute mon element li à l'element ul
 participantListElt.innerHTML=participantListElt.innerHTML + participantElt;
-
+// !!!!!!   Pour vider l'input sur l'HTML une fois que la saisie est faite  !!!!!
+nameinputElt.value = "";
 }
+const addNameFormeElt = document.getElementById("addNameForm");
+addNameFormeElt.addEventListener("submit" , addParticipant) ;
 
-addParticipant();
