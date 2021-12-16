03487f847ab6e14aef1ec1ee599f0f7fe197946a
MessineB <messinebelaroussi@gmail.com>
2021-12-16 10:43:48 +0100

    First commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+
+function addParticipant ()
+{
+
+//je créé mon prénom
+const participantName= "Prénom";
+//je créé un element li qui contient mon prenom
+const participantElt= `
+<li>${participantName}</li>
+`
+;
+//je récupère l' element ul qui contient la list des participants
+const participantListElt=document.getElementById("participantList");
+//j'ajoute mon element li à l'element ul
+participantListElt.innerHTML=participantListElt.innerHTML + participantElt;
+
+}
+
+addParticipant();
